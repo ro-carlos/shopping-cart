ca063f6c3ad1056184fcd920d4e3a25df11d2c8a
Carlos Andrés Rodríguez Torres <andresrodriguez@worldofficeonline.com>
2020-05-12 18:49:24 -0500

    Creación de componentes básicos

---
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss'],
+})
+export class CartComponent implements OnInit {
+  items = [1, 2, 3, 4];
+  constructor() {}
+
+  ngOnInit() {}
+}
