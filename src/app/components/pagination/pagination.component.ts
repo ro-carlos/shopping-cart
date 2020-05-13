490d4b05686077cb0b16bb3a27beaee052aeba44
Carlos Andrés Rodríguez Torres <andresrodriguez@worldofficeonline.com>
2020-05-13 17:02:32 -0500

    Generalización del componente de paginación

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,10 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Product } from 'src/app/models/product';
 import { ErrorService } from 'src/app/services/error.service';
 import { LoadingService } from 'src/app/services/loading.service';
-import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-pagination',
@@ -12,40 +11,36 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent implements OnInit {
-  @Input() requestPage: Observable<any>;
+  @Input() requestPage = (key: string, offset: number): Observable<any> => null;
   @Input() offset = 3;
-  @Output() appChangeValue: EventEmitter<Array<any>> = new EventEmitter();
 
   items: Array<any> = [];
   nextKey: string;
   prevKeys: Array<string> = [];
-  subscription: any;
 
   constructor(
     private loadingService: LoadingService,
-    private errorService: ErrorService,
-    private productService: ProductService
+    private errorService: ErrorService
   ) {}
 
   ngOnInit() {
-    this.getProducts('1');
+    this.getItems('1');
   }
 
   nextPage() {
     this.prevKeys.push(this.items[0].id + '');
-    this.getProducts(this.nextKey);
+    this.getItems(this.nextKey);
   }
 
   prevPage() {
     const prevKey = this.prevKeys[this.prevKeys.length - 1];
     this.prevKeys.pop();
-    this.getProducts(prevKey);
+    this.getItems(prevKey);
   }
 
-  getProducts(key?: string) {
+  getItems(key?: string) {
     this.loadingService.present();
-    this.productService
-      .getProducts(key, this.offset)
+    this.requestPage(key, this.offset)
       .pipe(
         map(this.errorService.detectGenericError),
         catchError(this.errorService.handleGenericError)
@@ -57,8 +52,7 @@ export class PaginationComponent implements OnInit {
           response.length - 1 === this.offset
             ? response[response.length - 1].id + ''
             : null;
-        console.log('RESPONSE products', response);
-        this.appChangeValue.emit(this.items);
+        console.log('RESPONSE items', response);
       });
   }
 }
--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { Product } from 'src/app/models/product';
-import { ErrorService } from 'src/app/services/error.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -8,25 +8,17 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   products: Array<Product> = [];
+  offset = 3;
 
-  constructor(
-    private productService: ProductService,
-    private errorService: ErrorService
-  ) {}
-
-  ngOnInit() {
-    this.init();
-  }
-
-  updateProducts(event) {
-    this.products = event;
-  }
+  constructor(private productService: ProductService) {}
 
   requestPage = (key: number, offset: number) => {
-    return this.productService.getProducts(key, offset);
+    return this.productService.getProducts(key, offset).pipe(
+      tap((response) => {
+        this.products = response.slice(0, this.offset);
+      })
+    );
   };
-
-  private init() {}
 }
